c95f1227b7c44319a6daf9f5ce13b61fc22d74d8
vchirikov <vovanchig@gmail.com>
2023-12-05 11:00:56 +0300

    add reading .env on pwsh start

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -370,6 +370,9 @@ function edgeDbg {
     & "runas" /trustlevel:0x20000 "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe --disable-background-networking --disable-background-timer-throttling --disable-backgrounding-occluded-windows --disable-breakpad --disable-client-side-phishing-detection --disable-default-apps --disable-dev-shm-usage --disable-renderer-backgrounding --disable-sync --metrics-recording-only --no-first-run --no-default-browser-check --remote-debugging-port=9222  --profile-directory=Default $url"
 }
 
+function todo {
+    & "Q:\tools\superproductivity\superProductivity.exe" "--user-data-dir=Q:\tools\superproductivity\data"
+}
 function removeFkingCreativeDrivers() {
     foreach ($dev in (Get-PnpDevice | Where-Object { $_.Name?.Contains("Sound Blaster", [System.StringComparison]::OrdinalIgnoreCase) -or ($_.Status -eq "Unknown" -and $_.Name?.Contains("Audio", [System.StringComparison]::OrdinalIgnoreCase) ) } )) {
         & "pnputil" /remove-device $dev.InstanceId
@@ -720,6 +723,80 @@ function kubeGenerateCrdJsonSchemas {
     [void]$sb.AppendLine("}");
     [void][System.IO.File]::WriteAllText("crd.json", $sb.ToString());
 }
+
+function readDotEnv {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [OutputType([System.Object[]])]
+    param(
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LiteralPath = (Join-Path -Path $PSScriptRoot -ChildPath '.env'),
+
+        [Parameter()]
+        [switch]
+        $Force
+    )
+
+    $linecursor = 0
+
+    if (Test-Path -LiteralPath $LiteralPath) {
+        Get-Content -LiteralPath $LiteralPath `
+        | ForEach-Object { # go through line by line
+            $line = $_.trim() # trim whitespace
+            if ($line -match "^\s*#") {
+                # it's a comment
+                Write-Verbose -Message "Found comment $line at line $linecursor. discarding"
+            }
+            elseif ($line -match "^\s*$") {
+                # it's a blank line
+                Write-Verbose -Message "Found a blank line at line $linecursor, discarding"
+            }
+            elseif ($line -match "^\s*(?<key>[^\n\b\a\f\v\r\s]+?)\s*=\s*(?<value>[^\n\b\a\f\v\r]*)$") {
+                # it's not a comment, parse it
+                # find the first '='
+                $key = $Matches["key"]
+                $value = $Matches["value"]
+
+                Write-Verbose -Message "Found [$key] with value [$value]"
+
+                # remove potential trailing comment
+                if (-not [string]::IsNullOrWhiteSpace($value)) {
+                    $idx = $value.IndexOf('#')
+                    if (-1 -lt $idx) {
+                        Write-Verbose -Message "`tRemoving trailing comment"
+                        $value = $value.Substring(0, $idx - 1).trimEnd()
+                    }
+                    $quote = $value[0]
+                    if ($quote -in "`"", "`'") {
+                        Write-Verbose -Message "`tQuoted value found, trimming quotes"
+                        $value = $value.trim($quote)
+                        Write-Verbose -Message "`tValue is now [$value]"
+                    }
+                }
+
+                if ($Force -or -not [System.Environment]::GetEnvironmentVariable($key, 'Process')) {
+                    if ($PSCmdlet.ShouldProcess("Environment variable [$key]", "Set value [$value]")) {
+                        [System.Environment]::SetEnvironmentVariable($key, $value, 'Process')
+                    }
+                }
+                else {
+                    Write-Verbose -Message "Environment variable $key already exists"
+                }
+            }
+            else {
+                Write-Warning "Invalid line $linecursor -> [$line]"
+            }
+            $linecursor++
+        }
+    }
+    else {
+        Write-Verbose "Dotenv file $LiteralPath not found."
+    }
+}
+
+# read env variables
+readDotEnv 'q:\code\vchirikov\dotfiles\secrets\.env'
 
 <#
 Helper function used to log parameters for tab completion scriptblock
